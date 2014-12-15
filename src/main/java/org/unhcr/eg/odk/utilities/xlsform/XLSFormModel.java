9fd5a289d25fec33aeb4d0cb47e164d6303919f8
MatayoBweta <MatayoBweta@gmail.com>
2014-12-15 20:23:22 +0200

    First commit

---
+++ src/main/java/org/unhcr/eg/odk/utilities/xlsform/XLSFormModel.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unhcr.eg.odk.utilities.xlsform;
+
+/**
+ *
+ * @author UNHCRuser
+ */
+public class XLSFormModel {
+
+    public enum Type_Field {
+
+        START("start"),
+        END("end"),
+        TODAY("today"),
+        DEVICE_ID("deviceid"),
+        SUBSCRIBER_ID("subscriberid"),
+        SIM_SERIAL("simserial"),
+        PHONE_NUMBER("phonenumber"),
+        BEGIN_GROUP("begin group"),
+        END_GROUP("end group"),
+        BEGIN_REPEAT("begin repeat"),
+        END_REPEAT("end repeat"),
+        INTEGER("integer"),
+        DECIMAL("decimal"),
+        TEXT("text"),
+        SELECT_ONE("select_one"),
+        SELECT_MULTIPLE("select_multiple"),
+        NOTE("note"),
+        GEO_POINT("geopoint"),
+        GEO_TRACE("geotrace"),
+        GEO_SHAPE("geoshape"),
+        DATE("date"),
+        TIME("time"),
+        DATE_TIME("dateTime"),
+        IMAGE("image"),
+        AUDIO("audio"),
+        VIDEO("video"),
+        BARCODE("barcode"),
+        CALCULATE("calculate");
+
+        private final String value;
+
+        public String value() {
+            return value;
+        }
+
+        Type_Field(String value) {
+            this.value = value;
+        }
+    }
+
+    public enum Appearance {
+
+        MULTILINE("multiline"),
+        MINIMAL("minimal"),
+        QUICK("quick"),
+        MONTH_YEAR("month-year"),
+        YEAR("year"),
+        HORIZONTAL_COMPACT("horizontal-compact"),
+        HORIZONTAL("horizontal"),
+        LIKERT("likert"),
+        QUICK_COMPACT("quickcompact"),
+        FIELD_LIST("field-list"),
+        LABEL("label"),
+        LIST_NOLABEL("list-nolabel"),
+        TABLE_LIST("table-list"),
+        SIGNATURE("signature"),
+        DRAW("draw");
+
+        private final String value;
+
+        public String value() {
+            return value;
+        }
+
+        Appearance(String value) {
+            this.value = value;
+        }
+    }
+
+    public enum SheetName {
+
+        SURVEY("survey"),
+        CHOICES("choices"),
+        SETTINGS("settings");
+
+        private final String value;
+
+        public String value() {
+            return value;
+        }
+
+        SheetName(String value) {
+            this.value = value;
+        }
+    }
+
+    public enum SheetColumn {
+
+        SURVEY_TYPE("type", SheetName.SURVEY),
+        SURVEY_NAME("name", SheetName.SURVEY),
+        SURVEY_DEFAULT("default", SheetName.SURVEY, true),
+        SURVEY_CHOICE_FILTER("choice_filter", SheetName.SURVEY, false, true),
+        SURVEY_HINT("hint", SheetName.SURVEY, true),
+        SURVEY_CONSTRAINT("constraint", SheetName.SURVEY, true),
+        SURVEY_CONSTRAINT_MESSAGE("constraint_message", SheetName.SURVEY, true),
+        SURVEY_APPEARANCE("appearance", SheetName.SURVEY, false, true),
+        SURVEY_RELEVANT("relevant", SheetName.SURVEY, false, true),
+        SURVEY_READ_ONLY("read_only", SheetName.SURVEY, false, true),
+        SURVEY_CALCULATION("calculation", SheetName.SURVEY, false, true),
+        SURVEY_LABEL("label", SheetName.SURVEY, true, false),
+        CHOICE_LIST_NAME("list_name", SheetName.CHOICES, false, false),
+        CHOICE_NAME("name", SheetName.CHOICES, false, false),
+        CHOICE_LABEL("label", SheetName.CHOICES, true, false),
+        SETTINGS_FORM_TITLE("form_title", SheetName.SETTINGS, false, false),
+        SETTINGS_FORM_ID("form_id", SheetName.SETTINGS, false, true),
+        SETTINGS_PUBLIC_KEY("public_key", SheetName.SETTINGS, false, false),
+        SETTINGS_SUBMISSION_URL("submission_url", SheetName.SETTINGS, false, false),
+        SETTINGS_DEFAULT_LANGUAGE("default_language", SheetName.SETTINGS, false, false);
+
+        private final String value;
+        private final SheetName sheetName;
+        private final boolean supportLangage;
+        private final boolean supportExpression;
+
+        public SheetName sheetName() {
+            return sheetName;
+        }
+
+        public String value() {
+            return value;
+        }
+
+        public boolean supportLangage() {
+            return supportLangage;
+        }
+
+        public boolean supportExpression() {
+            return supportExpression;
+        }
+
+        SheetColumn(String value, SheetName name) {
+            this(value, name, false, false);
+        }
+
+        SheetColumn(String value, SheetName name, boolean supportLangage) {
+            this(value, name, supportLangage, false);
+        }
+
+        private SheetColumn(String value, SheetName sheetName, boolean supportLangage, boolean supportExpression) {
+            this.value = value;
+            this.sheetName = sheetName;
+            this.supportLangage = supportLangage;
+            this.supportExpression = supportExpression;
+        }
+
+    }
+
+}
---
+++ src/main/java/org/unhcr/eg/odk/utilities/xlsform/excel/ExcelFileUtility.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unhcr.eg.odk.utilities.xlsform.excel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ *
+ * @author UNHCRuser
+ */
+public class ExcelFileUtility {
+
+    public static void addToDestination(HSSFWorkbook source, HSSFWorkbook destination) {
+        ArrayList<String> listOfSheet = getListOfSheets(destination);
+        int numberOfSheet = source.getNumberOfSheets();
+        int j = 0;
+        int sheetDestinationIndex;
+        for (int i = 0; i < numberOfSheet; i++) {
+            Sheet sheetSource = source.getSheetAt(i);
+            String newName = sheetSource.getSheetName();
+            sheetDestinationIndex = destination.getSheetIndex(newName);
+            while (listOfSheet.contains(newName)) {
+                newName = sheetSource.getSheetName() + "_" + j;
+                newName = newName.substring(Math.max(newName.length() - 31, 0), Math.min(newName.length(), 31));
+                sheetDestinationIndex = destination.getSheetIndex(newName);
+                System.out.println("----newName" + newName);
+                System.out.println("---------------sheetDestinationIndex " + sheetDestinationIndex);
+                j++;
+            }
+            listOfSheet.add(newName);
+            System.out.println("----newName" + newName);
+            Sheet sheetDestination = destination.createSheet(newName);
+            copyContent(sheetSource, sheetDestination);
+        }
+    }
+
+    protected static ArrayList<String> getListOfSheets(HSSFWorkbook workbook) {
+        ArrayList<String> listOfSheets = new ArrayList<>();
+        int numberOfSheet = workbook.getNumberOfSheets();
+        for (int i = 0; i < numberOfSheet; i++) {
+            listOfSheets.add(workbook.getSheetName(i));
+        }
+        return listOfSheets;
+    }
+
+    protected static void copyContent(Sheet sheetSource, Sheet sheetDestination) {
+        //Iterate through each rows from first sheet
+        Iterator<Row> rowIterator = sheetSource.iterator();
+        int i = 0;
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Row rowDestination = sheetDestination.createRow(i);
+            i++;
+            //For each row, iterate through each columns
+            Iterator<Cell> cellIterator = row.cellIterator();
+            int j = 0;
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                Cell cellDestination = rowDestination.createCell(j);
+                j++;
+                cellDestination.setCellComment(cell.getCellComment());
+//                cellDestination.setCellStyle(cell.getCellStyle());
+                cellDestination.setCellType(cell.getCellType());
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        cellDestination.setCellValue(cell.getBooleanCellValue());
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            cellDestination.setCellValue(cell.getDateCellValue());
+                        } else {
+                            cellDestination.setCellValue(cell.getNumericCellValue());
+                        }
+                        break;
+                    case Cell.CELL_TYPE_STRING:
+                        cellDestination.setCellValue(cell.getRichStringCellValue());
+                        break;
+                    case Cell.CELL_TYPE_BLANK:
+                        cellDestination.setCellValue(cell.getStringCellValue());
+                        break;
+                    case Cell.CELL_TYPE_ERROR:
+                        cellDestination.setCellValue(cell.getErrorCellValue());
+                        break;
+                    case Cell.CELL_TYPE_FORMULA:
+                        cellDestination.setCellFormula(cell.getCellFormula());
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+}
